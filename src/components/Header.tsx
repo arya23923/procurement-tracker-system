e29e478a96e59157567a11d7accbb1613bcf159d
Arya Gupta <aryagupta239@gmail.com>
2025-08-03 19:13:12 +0530

    header almost done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,12 @@
 import { FC } from 'react';
-// import { FaBars } from 'react-icons/fa';
-import Sidebar from 'src/components/images/sidebar.svg'
+import Sidebar from './images/sidebar.svg';
 
 const Header: FC = () => {
   return (
     <header className='flex justify-between items-center w-full'>
-      <div className='flex'>
-        <img className='w-18 pr-8' src={Sidebar} alt="sidebar" />
-        <div className='text-4xl'>Procurement Tracker</div>
+      <div className='flex w-100'>
+        <img src={Sidebar} alt="sidebar" className="w-13 pr-4" />
+        <div className='text-4xl font-semibold'>Procurement Tracker</div>
       </div>
       <div>
           <button className='bg-blue-500 w-20 p-2 text-white rounded-lg text-lg'>Log out</button>
