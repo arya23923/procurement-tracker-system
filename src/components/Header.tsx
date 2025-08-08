1a7fa40438d58d07c3512129eb571b11e0fac536
Arya Gupta <aryagupta239@gmail.com>
2025-08-08 23:22:29 +0530

    shortcut section file created

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ const Header: FC = () => {
       <div className='flex w-100'>
         <div className='text-xl md:text-4xl font-semibold p-0 md:pl-15 md:mt-5 md:p-0 md:w-50 whitespace-nowrap'>Procurement Tracker</div>
       </div>
-      <div className='flex justify-between gap-x-2 md:gap-x-5'>
+      <div className='flex justify-between gap-x-2 md:gap-x-10'>
           <button onClick={() => setModal(!isModal)} className='bg-gray-400 md:w-11 w-10 border rounded-full border-gray-400 m-5 flex justify-center items-center'>
             <img src={Profileicon} alt="profile_icon" className='w-6'/>
           </button>
