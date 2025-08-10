116c169bf31eb9e596f245a262ff8eeafffb0d4f
Arya Gupta <aryagupta239@gmail.com>
2025-08-10 18:50:46 +0530

    fully functioning shortcut with key notes

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -2,8 +2,13 @@ import { FC } from "react";
 
 const Key:FC = () => {
     return(
-        <div className="border border-lg border-gray-300">
-            <p>Key Notes</p>
+        <div className="border rounded-lg border-gray-300">
+            <div className="flex justify-between p-3">
+                <p className="font-semibold md:text-2xl flex items-center">Key Notes</p>
+                <button className="bg-blue-500 text-white rounded-lg p-2 text-sm md:text-lg">Save</button>
+            </div>
+            <textarea spellCheck={false} name="postContent" placeholder="Write your thoughts here..." rows={5} className="rounded-lg border border-gray-200 
+            w-100 text-gray-500 p-2 md:w-140 focus:text-gray-800 focus:border-gray-500"/>
         </div>
     )
 }
