ae95ba9409517912e09d4998f60c171c7d56189d
Arya Gupta <aryagupta239@gmail.com>
2025-08-10 19:45:13 +0530

    notification panel added with responsiveness

---
+++ src/components/Notification.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const Notification: FC = () => {
+    return(
+        <div className="flex flex-col items-start p-0 pt-5 pb-5">
+            <p className="font-semibold p-4 md:text-2xl">Notification Panel</p>
+            <div className="p-15 pr-20 pl-20 border border-gray-300 rounded-lg w-80 pb-10 md:w-full md:p-20"></div>
+        </div>
+    )
+}
+
+export default Notification;
