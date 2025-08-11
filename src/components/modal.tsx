3cf7c91093cde4a60b2d082e11a845feb6c70f1f
Arya Gupta <aryagupta239@gmail.com>
2025-08-11 23:18:36 +0530

    done

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -9,7 +9,7 @@ const Modal : FC<ModalProps> = ({onClose}) => {
          <div className="fixed inset-0 bg-black/75 z-10 flex items-center justify-center">
             <div className="bg-white flex justify-between items-start p-6 rounded-lg w-70 h-50 md:w-100 md:h-80">
                 <p className="p-2 md:text-3xl">User details</p>
-                <button onClick={onClose} className="bg-gray-500 w-10 rounded-full p-2 md:text-xl">x</button>
+                <button onClick={onClose} className="bg-gray-500 w-10 rounded-full p-2 md:text-xl md:w-11">x</button>
             </div>
         </div>
     )
