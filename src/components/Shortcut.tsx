d36d599dd1bcbebf86d1aa427f1130713943e5ac
Arya Gupta <aryagupta239@gmail.com>
2025-08-13 13:27:25 +0530

    Shortcut section modified in accordance to layout sent

--- src/components/Shortcut.tsx
+++ src/components/Shortcut.tsx
@@ -3,11 +3,11 @@ import { FC } from "react";
 const Shortcut :FC = () => {
     return(
         <div className="w-full flex flex-col">
-            <p className="p-5 flex items-start font-semibold md:text-2xl">Shortcut Section</p>
-            <div className="flex justify-around items-center border border-gray-300 p-2 rounded-lg text-sm md:text-xl md:p-5 md:w-140">
-                <a href="#" className="bg-gray-200 p-3 rounded-lg">Pending List</a>
-                <a href="#" className="bg-gray-200 p-3 rounded-lg">Raise Request</a>
-                <a href="#" className="bg-gray-200 p-3 rounded-lg md:pr-8 md:pl-8">MIS</a>
+            <p className="p-5 bg-emerald-600 shadow-lg shadow-emerald-100 text-white border border-white rounded-lg flex items-start font-semibold md:w-140 md:text-2xl">Shortcut Section</p>
+            <div className="flex justify-around items-center border border-emerald-500 shadow-lg shadow-emerald-100 p-2 rounded-lg text-sm md:text-xl md:p-5 md:w-140">
+                <a href="#" className="bg-emerald-200 p-3 rounded-lg">Pending List</a>
+                <a href="#" className="bg-emerald-200 p-3 rounded-lg">Raise Request</a>
+                <a href="#" className="bg-emerald-200 p-3 rounded-lg md:pr-8 md:pl-8">MIS</a>
             </div>
         </div>
     )
