53aa202beac133b2819b175080fae0123f068f7b
Arya Gupta <aryagupta239@gmail.com>
2025-08-13 18:59:06 +0530

    messed with the distance between the user profile button and log out button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header: FC = () => {
   return (
     <header className='flex justify-between items-center w-full pt-0 pb-3 md:pt-4 md:pb-10'>
       <div className='text-xl md:text-4xl pr-2 font-semibold p-0 md:pl-10 md:mt-5 md:p-0 md:w-50 whitespace-nowrap'>Procurement Tracker</div>
-      <div className='flex justify-between gap-x-1 md:gap-x-10'>
+      <div className='flex justify-between gap-x-1 md:gap-x-8'>
           <button onClick={() => setModal(!isModal)} className='bg-gray-400 md:w-11 md:h-11 w-8 h-8 border rounded-full border-gray-400 flex justify-center items-center'>
             <img src={Profileicon} alt="profile_icon" className='w-4 md:w-6'/>
           </button>
